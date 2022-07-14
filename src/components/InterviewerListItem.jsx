44436e4a6bf61ac1e9e729583a8525aa2d7bc998
Jesse Shen <jly.shen@gmail.com>
2022-07-13 19:31:05 -0700

    added InterviewerList

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,42 @@
+/*
+Therefore, the <InterviewerListItem> component should receive the following props:
+
+id:number - the id of the interviewer
+name:string - the name of the interviewer
+avatar:url - a url to an image of the interviewer
+The <InterviewerListItem> also needs a prop to know if it is selected.
+
+selected:boolean - determines if an interviewer is selected or not and displays the name and applies appropriate styles if selected.
+Finally, the <InterviewerListItem> should receive a function called setInterviewer.
+
+setInterviewer:function - is run when the <InterviewerListItem> is clicked. This function receives the interviewer's id as an argument. It sets the selected interviewer.
+
+*/
+
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+
+export default function InterviewerListItem(props) {
+
+  const InterviewerClass = classNames("interviewers__item",
+  {
+    "interviewers__item--selected":props.selected
+  })
+
+  return (
+    <li
+      className= {InterviewerClass}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected ? <>{props.name}</> : ""}
+    </li>
+  );
+}
